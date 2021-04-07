aa944034a8c0a064e39968d33717d9610eeaee3a
nju16szh <nju16szh@gmail.com>
2021-04-07 18:08:02 +0800

    1

--- 3.无重复字符的最长子串.cpp
+++ 3.无重复字符的最长子串.cpp
@@ -4,13 +4,16 @@
  * [3] 无重复字符的最长子串
  */
 
-// @lc code=start
-class Solution {
+// @lc code=startF
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        int a;
-        int b;
+    int lengthOfLongestSubstring(string s)
+    {
+        set<char> c;
+        for (int i = 0; i < s.size(); i++)
+            c.insert(s[i]);
+        return c.size();
     }
 };
 // @lc code=end
-
